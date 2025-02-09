e7c2af442899363a18e967afc34512c9de65e609
prajapattiravi37 <prajapatiravi37@gmail.com>
2025-02-09 16:59:06 +0530

    Initial commit

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -27,7 +27,7 @@ class Order extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class); 
     }
 
     public function dealer()
